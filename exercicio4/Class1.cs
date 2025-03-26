bcff3e53ef6adcfd433aa3248a84ce5afc18a5b4
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-03-26 20:53:50 -0300

    Add files via upload

---
+++ exercicio4/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    internal class Lampada
+    {
+    private bool Ligada;
+
+    public void Ligar()
+    {
+        Ligada = true;
+        Console.WriteLine("A lâmpada foi ligada.");
+    }
+
+    public void Desligar()
+    {
+        Ligada = false;
+        Console.WriteLine("A lâmpada foi desligada.");
+    }
+
+    public void MostrarEstado()
+    {
+        if (Ligada)
+            Console.WriteLine("A lâmpada está ligada.");
+        else
+            Console.WriteLine("A lâmpada está desligada.");
+    }
+}
+
---
+++ exercicio4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Lampada lampada = new Lampada();
+        int opcao;
+
+        do
+        {
+            Console.WriteLine("\nMenu da Lâmpada:");
+            Console.WriteLine("1 - Ligar a lâmpada");
+            Console.WriteLine("2 - Desligar a lâmpada");
+            Console.WriteLine("3 - Mostrar estado da lâmpada");
+            Console.WriteLine("0 - Sair");
+            Console.Write("Escolha uma opção: ");
+
+            if (int.TryParse(Console.ReadLine(), out opcao))
+            {
+                switch (opcao)
+                {
+                    case 1:
+                        lampada.Ligar();
+                        break;
+                    case 2:
+                        lampada.Desligar();
+                        break;
+                    case 3:
+                        lampada.MostrarEstado();
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida! Digite um número.");
+            }
+        } while (opcao != 0);
+    }
+}
