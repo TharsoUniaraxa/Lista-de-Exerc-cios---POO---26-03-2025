aa537afe73072d1eca26929c7d07a06d014230b4
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-03-26 21:20:14 -0300

    Add files via upload

---
+++ exercicio1/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+class Aluno
+{
+    public string ra;
+    public string nome;
+    public double notaprova;
+    public double notatrabalho;
+    public double notafinal;
+
+
+
+
+
+
+
+    public void dados()
+    {
+
+        Console.WriteLine("Escreva o RA");
+
+        ra = Console.ReadLine();
+
+        Console.WriteLine("Escreva o nome");
+
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Escreva o valor da nota");
+
+        notaprova = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Escreva o valor da nota");
+
+        notatrabalho = Convert.ToDouble(Console.ReadLine());
+
+
+    }
+    public void media()
+    {
+        notafinal = (notaprova + notatrabalho) / 2;
+
+    }
+
+    public void notaf()
+
+    {
+
+        if (notafinal >= 6.0)
+
+        {
+
+            Console.WriteLine($" {nome} {ra} aprovado, média {notafinal:F2}.");
+
+        }
+        else
+        {
+            double notamedia = 12;
+
+
+            notamedia = 12 - notafinal;
+
+
+            Console.WriteLine($"aluno {nome} (ra: {ra}) precisa de {notamedia:F2} na prova final.");
+        }
+    }
+}
+
+
+
+
---
+++ exercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aluno aluno = new Aluno();
+
+        aluno.dados();
+        aluno.media();
+        aluno.notaf();
+    }
+}
