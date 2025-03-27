5a732eec95ae3d689593989dda1f1139b43c1e67
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-03-26 21:17:01 -0300

    Add files via upload

---
+++ exercicio3/Class1.cs
@@ -0,0 +1,38 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+        class Produto
+        {
+            public string nome;
+            public double preço;
+            public int quantidade;
+
+            public void adicionarestoque(int adicionarquantidade)
+            {
+                quantidade += adicionarquantidade;
+            }
+
+            public void removerestoque(int removerquantidade)
+            {
+                if (removerquantidade <= quantidade)
+                {
+                    quantidade -= removerquantidade;
+                }
+                else
+                {
+                    Console.WriteLine("quantidade acima do estoque");
+                }
+            }
+
+            public void exibirdetalhes()
+            {
+                Console.WriteLine($"Produto: {nome}");
+                Console.WriteLine($"Preço: {preço}");
+                Console.WriteLine($"Quantidade em estoque: {quantidade}");
+            }
+        }
+
+
---
+++ exercicio3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+
+
+
+
+
+{
+
+    static void Main()
+
+    {
+        Produto produto = new Produto();
+
+
+
+        Console.Write("escreva o produto: ");
+        produto.nome = Console.ReadLine();
+        Console.Write("escreva o preço: ");
+        produto.preço = Convert.ToDouble(Console.ReadLine());
+        Console.Write("escreva a quantidade do estoque: ");
+        produto.quantidade = int.Parse(Console.ReadLine());
+
+
+
+        while (true)
+
+
+        {
+            Console.WriteLine("Escolha uma opçao:");
+            Console.WriteLine("1 = Adicionar ao estoque");
+            Console.WriteLine("2 = Remover do estoque");
+            Console.WriteLine("3 = Exibir detalhes");
+            int opcao = int.Parse(Console.ReadLine());
+
+
+            if (opcao == 1)
+
+            {
+
+                Console.Write("Digite a quantidade que você deseja adicionar ao estoque: ");
+                int quantidade = int.Parse(Console.ReadLine());
+                produto.adicionarestoque(quantidade);
+
+            }
+            else if (opcao == 2)
+            {
+                Console.Write("Digite a quantidade que você deseja remover do estoque: ");
+
+
+                int quantidade = int.Parse(Console.ReadLine());
+                produto.removerestoque(quantidade);
+            }
+            else if (opcao == 3)
+            {
+                produto.exibirdetalhes();
+            }
+        }
+    }
+}
