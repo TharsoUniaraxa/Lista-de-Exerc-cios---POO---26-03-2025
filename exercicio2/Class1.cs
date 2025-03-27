4b2385692b88ad64aa3b15fac235c674dd21b19f
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-03-26 21:18:21 -0300

    Add files via upload

---
+++ exercicio2/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Contabancaria
+{
+    public string numeroconta;
+    public string titular;
+    public double saldo;
+
+
+
+
+
+    public void Depositar(double valor)
+    {
+        if (valor > 0) 
+        {
+            saldo += valor; 
+            Console.WriteLine($"R${valor} depositado");
+        }
+    }
+
+    public void sacar(double valor)
+    {
+        if (valor > 0) 
+        {
+            if (saldo >= valor) 
+
+            {
+
+                saldo -= valor;
+
+                Console.WriteLine($"R${valor} sacado");
+
+
+            }
+            else
+            {
+
+                Console.WriteLine("Não tem saldo");
+            }
+        }
+    }
+    public void exibir()
+
+    {
+        Console.WriteLine($"R${saldo}");
+
+    }
+
+}
---
+++ exercicio2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Contabancaria conta = new Contabancaria();
+
+            
+        int opcao = 0;
+
+        while (opcao != 4) 
+        {
+
+
+            Console.WriteLine("Opções:");
+            Console.WriteLine("1 - Depósito");
+            Console.WriteLine("2 - Saque");
+            Console.WriteLine("3 - Exibir saldo");
+
+            Console.Write("Escolha uma opção: ");
+            opcao = int.Parse(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case 1:
+                    Console.Write("Digite o valor do deposito: ");
+                    double valorDeposito = double.Parse(Console.ReadLine());
+                    conta.Depositar(valorDeposito);
+                    break;
+                    
+                case 2:
+                    Console.Write("Digite o valor do saque: ");
+                    double valorSaque = double.Parse(Console.ReadLine());
+                    conta.sacar(valorSaque);
+                    break;
+                case 3:
+                    conta.exibir();
+                    break;
+                default:
+                    Console.WriteLine("tente outra opção");
+                    break;
+
+            }
+
+        } 
+    }
+}
